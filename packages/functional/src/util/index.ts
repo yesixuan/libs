8ff631e53cc51c9801d75a0310042d4b4160b549
yesixuan <18373209286@163.com>
2020-11-11 15:23:30 +0800

    feat: 引入ts-toolbelt定义ts 类型，重构curry 与 compose 函数类型定义

--- packages/functional/src/util/index.ts
+++ packages/functional/src/util/index.ts
@@ -1,4 +1,5 @@
 import { Left, Right } from ".."
+import { F } from 'ts-toolbelt'
 
 interface anyFn {
   (...args: any): any
@@ -13,17 +14,17 @@ export function partial<T extends anyFn> (fn: T, ...presetArgs): partialType<Ret
     fn(...presetArgs, ...laterArgs)
 }
 
-export const curry = (fn, arity: number = fn.length, nextCurried?: any) => 
-  (nextCurried = prevArgs => 
-    (...nextArgs) => {
-      const args = [ ...prevArgs, ...nextArgs ]
-      return args.length >= arity
-        ? fn(...args)
-        : nextCurried(args)
+export function curry<Fn extends F.Function>(cb: Fn): F.Curry<Fn> {
+  return (...args) => {
+    if (args.length < cb.length) {
+      return curry.bind(cb.bind(null, ...args))
+    } else {
+      return cb(...args)
     }
-  )([])
+  }
+}
 
-export const compose = (...fns) => 
+export const compose: F.Compose = (...fns) => 
   fns.length <= 1
     ? fns[0]
     : fns.reduce((prev, curr) => (...args) => prev(curr(...args)))
@@ -36,10 +37,4 @@ export const either = curry((leftFn, rightFn, functor) => {
 })
 
 
-var haha = curry(
-  (a, b): number => a + b
-)
-var hehe = haha(3)
-
-console.log(hehe);
 
